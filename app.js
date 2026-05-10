b1d1ceadd77929e62a329edd380e0a97b7150784
Bukola Laniyan <105382504+khenzee@users.noreply.github.com>
2026-05-10 16:00:46 +0100

    Add basic HTML page and demo JavaScript
    Add page.html and app.js. page.html loads app.js; app.js sets page styles and inner HTML and includes simple JavaScript examples (let/const, primitive types, and console.log statements) intended as a small demo for learning basic DOM and JS concepts.

---
+++ app.js
@@ -0,0 +1,29 @@
+document.body.style.backgroundColor = "blue";
+document.body.style.color = "white";
+document.body.innerHTML = "<h1>Hello World</h1>"
+
+// Variables let and const.
+let number = 10;
+number = 20
+
+console.log(number)
+
+const greetings = "hello world"
+console.log(greetings)
+console.log(typeof(greetings))
+// Data Type. this is classified into primitive and reference data type. but today we will be working on the primitive data types.
+
+//primitive data type
+
+//string example
+let Name = "Emma" //string
+console.log(typeof(Name))
+
+//number
+let num = 50
+
+//boolean
+
+//undefined
+
+//null
